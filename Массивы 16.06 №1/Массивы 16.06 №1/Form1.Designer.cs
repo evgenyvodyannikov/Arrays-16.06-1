ca98aead693d37eae8e2f2ac81d4224578ad34dd
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-16 12:12:37 +0500

    Конец

--- Массивы 16.06 №1/Массивы 16.06 №1/Form1.Designer.cs
+++ Массивы 16.06 №1/Массивы 16.06 №1/Form1.Designer.cs
@@ -30,38 +30,155 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tB1 = new System.Windows.Forms.TextBox();
+            this.tB2 = new System.Windows.Forms.TextBox();
+            this.tB3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(292, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(359, 267);
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(129, 267);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(518, 274);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(658, 153);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(122, 57);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(658, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Найти";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tB3);
+            this.groupBox1.Controls.Add(this.tB2);
+            this.groupBox1.Controls.Add(this.tB1);
+            this.groupBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 267);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные массива";
+            // 
+            // tB1
+            // 
+            this.tB1.Location = new System.Drawing.Point(114, 42);
+            this.tB1.MaxLength = 3;
+            this.tB1.Name = "tB1";
+            this.tB1.Size = new System.Drawing.Size(48, 33);
+            this.tB1.TabIndex = 0;
+            this.tB1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tB1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tB2
+            // 
+            this.tB2.Location = new System.Drawing.Point(168, 42);
+            this.tB2.MaxLength = 3;
+            this.tB2.Name = "tB2";
+            this.tB2.Size = new System.Drawing.Size(48, 33);
+            this.tB2.TabIndex = 0;
+            this.tB2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tB2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // tB3
+            // 
+            this.tB3.Location = new System.Drawing.Point(189, 81);
+            this.tB3.MaxLength = 3;
+            this.tB3.Name = "tB3";
+            this.tB3.Size = new System.Drawing.Size(62, 33);
+            this.tB3.TabIndex = 0;
+            this.tB3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tB3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кол-во элементов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Диапазон:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(427, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(366, 75);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "В целочисленном массиве найти\r\nнаиболее длинную цепочку \r\nподряд идущих одинаковы" +
+    "х элементов";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(792, 287);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +186,14 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tB3;
+        private System.Windows.Forms.TextBox tB2;
+        private System.Windows.Forms.TextBox tB1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Массивы 16.06 №1/Массивы 16.06 №1/Form1.cs
+++ Массивы 16.06 №1/Массивы 16.06 №1/Form1.cs
@@ -17,19 +17,107 @@ namespace Массивы_16._06__1
         {
             InitializeComponent();
         }
-        int[] mas = new int[50];
+        int[] mas;
+        int start;
+        int end;
         private void button1_Click(object sender, EventArgs e)
         {
-            dataGridView1.Rows.Clear();
-            dataGridView1.Columns.Clear();
-            dataGridView1.Columns.Add("Эл. массива", "Эл. массива");
-            dataGridView1.Rows.Add(50);
-            Random r = new Random();
-            for(int i = 0; i < mas.Length; i++)
+            try
             {
-                mas[i] = r.Next(-20, 20);
-                dataGridView1.Rows[i].Cells[0].Value = mas[i];
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView1.Columns.Add("Эл. массива", "Эл. массива");
+                dataGridView1.Rows.Add(int.Parse(tB3.Text));
+                Random r = new Random();
+                mas = new int[int.Parse(tB3.Text)];
+                start = int.Parse(tB1.Text);
+                end = int.Parse(tB2.Text);
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] = r.Next(start, end);
+                    dataGridView1.Rows[i].Cells[0].Value = mas[i];
+                }
+                button2.Enabled = true;
             }
+            catch { 
+                if (start > end)
+                    MessageBox.Show("Начало диапазона должно быть меньше конца", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                MessageBox.Show("Введите данные для создания массива!!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int k = 1;
+            int max = 0;
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if(mas[i-1] == mas[i])
+                { k++; }
+                else
+                { k = 1; }
+                if(k > max)
+                { max = k;}    
+            }
+            MessageBox.Show("Длина цепочки повторяющихся элементов равна " + max.ToString(), "Ответ:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                if (tb.Text.Length == 3 && tb.Text.Length <= 3)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (e.KeyChar == '-')
+            {
+                if ((tB1.Text.IndexOf('-') != -1) || (tB1.Text.Length == 1))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Back)
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            try
+            {
+                switch (tb.Name)
+                {
+                    case "tB1":
+                        { 
+                            if (int.Parse(tB1.Text) < -50) tB1.Text = "-50";
+                            if (int.Parse(tB1.Text) >  50) tB1.Text = "50";
+                            if (int.Parse(tB1.Text) > int.Parse(tB2.Text)) tB1.Text = (int.Parse(tB2.Text) - 1).ToString();
+                        }
+                        break;
+                    case "tB2":
+                        {
+                            if (int.Parse(tB2.Text) > 50) tB2.Text = "50";
+                            if (int.Parse(tB1.Text) < -50) tB1.Text = "-50";
+                        }
+                        break;
+                    case "tB3":
+                        { if (int.Parse(tB3.Text) > 250) tB3.Text = "250"; }
+                        break;
+                }
+            }
+            catch { }
         }
     }
 }
