80bb63b17d0384bb9e021b1672234383b1ddeed8
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-16 11:32:45 +0500

    Вывод в DGV

--- Массивы 16.06 №1/Массивы 16.06 №1/Form1.cs
+++ Массивы 16.06 №1/Массивы 16.06 №1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,20 @@ namespace Массивы_16._06__1
         public Form1()
         {
             InitializeComponent();
+        }
+        int[] mas = new int[50];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Эл. массива", "Эл. массива");
+            dataGridView1.Rows.Add(50);
+            Random r = new Random();
+            for(int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(-20, 20);
+                dataGridView1.Rows[i].Cells[0].Value = mas[i];
+            }
         }
     }
 }
